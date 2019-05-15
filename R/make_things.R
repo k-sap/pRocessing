fff08205e7a16a22f9a18fb39871b624f33939f1
k-sap <karlo975@hotmail.com>
2019-05-15 11:23:37 +0200

    init

---
+++ R/make_things.R
@@ -0,0 +1,22 @@
+#' @title pRocessing
+#' @description
+#' It gives R and the user possibility to contemplate and pRocess the input or a thought given.
+#'
+#' @param words That is one argument which, at least should, be a character string.
+#'
+#' @exapmle
+#'
+make_things <- function(words = "Hi!"){
+
+cat(paste0("-------
+< ", words,
+" >
+-------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+  "))
+}
+
